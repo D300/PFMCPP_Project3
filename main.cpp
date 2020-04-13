7a0027af96d41e34af5bb92307cf64b069ce1309
matkatmusic <matkatmusic@gmail.com>
2020-04-13 15:08:46 -0700

    added part3 instructions

--- main.cpp
+++ main.cpp
@@ -1,98 +1,63 @@
- /*
- Project 3 - Part 2 / 5
- Video: Chapter 2 Part 6
- Implementations tasks
+/*
+ Project 3 - Part 3 / 5
+ video: Chapter 2 - Part 8
+ Constructors tasks
 
-Create a branch named Part2
+ Create a branch named Part3
 
- tasks
- 0) delete all of the plain english pseudo-code you added in Part1.
-   - you should be left with only your UDTs.
+ On a new branch:
 
- 1) write the definition for the Type that leftFoot and rightFoot are instantiations of.
-    don't forget to define and implement the member functions 'stepForward()' and 'stepSize()'
-    you should be able to deduce the return type of those functions based on their usage in Person::run()
-    You'll need to insert the Person struct from the video in the space below.
- */
-
-struct Foot
-{
-    float footSize;
-    int numSteps = 0;   
-
-    void stepForward();
-    int stepSize(int size);
-};
-
-int Foot::stepSize(int size_)
-{
-    return size_;
-}
-
-void Foot::stepForward()
-{
-    numSteps++;
-}
-
-
-struct Person
-{
-    int age;
-    int heigth;
-    float hairlength;
-    float GPA;
-    unsigned int SATScore;
-
-    int distanceTraveled;
-
-    void run(int stepSize, bool startWithLeftFoot);
-
-    Foot leftFoot;
-    Foot rightFoot;
-};
-
-
-void Person::run(int stepSize, bool startWithLeftFoot)
-{
-    if (startWithLeftFoot)
-    {
-        leftFoot.stepForward();
-        rightFoot.stepForward();
-    }
-    else
-    {
-        rightFoot.stepForward();
-        leftFoot.stepForward();
-    }
-
-    distanceTraveled += leftFoot.stepSize(stepSize) + rightFoot.stepSize(stepSize);
-}
-
- /*
- 2) provide implementations for the member functions you declared in your 10 user-defined types from the previous video outside of your UDT definitions.
- x
-
- 3) be sure to write the correct full qualified name for the nested type's member functions.
- x
+ 1) Add a constructor for each User-Defined-Type.
  
- 4) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
- x
+ 2) amend some of your UDT's member functions to print out something interesting via std::cout
+ 
+ 3) Instantiate 1 or 2 instances of each of your user-defined types in the main function at the bottom of that file,
+
+ 4) call some of your UDT's amended member functions in main().
+ 
+ 5) add some std::cout statements in main() that print out your UDT's member variable values or values returned from your UDT member functions (if they return values)
+ 
+ After you finish defining each type/function:
+ click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
-Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+ Make a pull request after you make your first commit and pin the pull request link to our DM thread.
 
-send me a DM to check your pull request
+ send me a DM to check your pull request
 
  Wait for my code review.
+ 
+ example:
  */
 
-/*
- Checkpoint 1:
- Fill in #1 - 4 with a random UDT in plain english
- These 4 UDTs do not need to be related.
- For each plain-english UDT, write out 3 things it can do, and 5 traits or properties.
- */
+#include <iostream>
+namespace Example 
+{
+struct UDT  // my user defined type named 'UDT'
+{
+    int a; //a member variable
+    UDT() { a = 0; }             //1) the constructor
+    void printThing()            //the member function
+    {
+        std::cout << "UDT::printThing() " << a << std::endl;  //2) printing out something interesting
+    }
+};
+
+int main()
+{
+    UDT foo;              //3) instantiating a UDT named 'foo' in main()
+    foo.printThing();     //4) calling a member function of the UDT instance.
+    
+    //5) a std::cout statement accessing foo's member variable.
+    //It also demonstrates a 'ternary expression', which is syntactic shorthand for an 'if/else' expression
+    std::cout << "Is foo's member var 'a' equal to 0? " << (foo.a == 0 ? "Yes" : "No") << "\n";
+    
+    return 0;
+}
+}
+
+//insert Example::main() into main() of user's repo.
 
 #include <iostream>
 using namespace std;
@@ -381,7 +346,7 @@ void TimeWarpFlexThing::Pattern::permutatePatternOverLen(int patternLen_)
 
 
 //=========================================================
-struct  InsaneMusicMachine 
+struct InsaneMusicMachine 
 {
     AudioInterface ioDevice;
     DrumPads drumPads;
@@ -395,18 +360,10 @@ struct  InsaneMusicMachine
 };
 
 //=========================================================
-#include <iostream>
-
-template<typename ...T>
-void ignoreUnused(T&&...) { }
 
 int main()
 {
+    Example::main();
     std::cout << "good to go!" << std::endl;
-
-    // ignoreUnused(audio, unused, bufSize);
-
-    Person p;
-    p.run(3, true);
 }
 
